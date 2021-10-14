933f5c1e60b10ac088f2bbc074b8de88b6dda5c9
James Liang <james.liang.cje@icloud.com>
2021-10-14 19:08:10 -0400

    updated with simpler autovalidator

--- {{cookiecutter.question_source}}/q-{{cookiecutter.question_id}}/validate_auto.sh
+++ {{cookiecutter.question_source}}/q-{{cookiecutter.question_id}}/validate_auto.sh
@@ -1,7 +1,3 @@
 g++ -o submission submission_{{cookiecutter.question_id}}.cpp
-OUTPUT=$(cat input.txt | ./submission)
-echo "=========================================="
-echo "============== OUTPUT ===================="
-echo $OUTPUT
-echo "===============END OF OUTPUT==============="
-echo $OUTPUT > produced_output.txt
+echo "==== OUTPUT ====="
+cat input.txt | ./submission
