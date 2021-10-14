3b097771a9c3a36e926911ff6ab9a11ff5060aa4
James Liang <james.liang.cje@icloud.com>
2021-10-14 18:56:44 -0400

    updated and removed diffing

--- {{cookiecutter.question_source}}/q-{{cookiecutter.question_id}}/validate_auto.sh
+++ {{cookiecutter.question_source}}/q-{{cookiecutter.question_id}}/validate_auto.sh
@@ -5,6 +5,3 @@ echo "============== OUTPUT ===================="
 echo $OUTPUT
 echo "===============END OF OUTPUT==============="
 echo $OUTPUT > produced_output.txt
-echo "=============== DIFF ======================"
-git diff --no-index --color-words produced_output.txt output.txt
-echo "============= END OF DIFF ================="
